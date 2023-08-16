b7d55ded14bee2a134867762dad342fbee3a13f6
Stanislav Zaichenko <stanislav.zaichenko@marem.io>
2023-08-16 12:09:50 +1000

    styles updates

--- src/components/ui/LabelInput.tsx
+++ src/components/ui/LabelInput.tsx
@@ -30,14 +30,9 @@ export const LabelInput = ({
       </div>
       <label htmlFor={name}>{label}</label>
       <div className="mt-4 mb-8 grid grid-cols-[112px_auto] gap-8">
-        <input
-          name={`current-${name}`}
-          autoComplete="none"
-          type="text"
-          disabled
-          className="rounded border border-[#5A5597] bg-[#38346A] text-[16px] text-white w-full h-[56px] pl-6 pr-4 focus:outline-none"
-          value={`$ ${defaultValue}`}
-        />
+        <span className="rounded border border-[#5A5597] bg-[#38346A] text-[16px] text-white w-full h-[56px] leading-[56px] pl-6 pr-4 focus:outline-none">
+          $ {defaultValue}
+        </span>
         <input
           onChange={onInputChange}
           id={name}
