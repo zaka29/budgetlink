906fd83f93bdb8b355a54acc6da18e3b99d3e672
Stanislav Zaichenko <stanislav.zaichenko@marem.io>
2023-08-28 14:45:57 +1000

    updates

--- src/components/AddToSpreadsheet/AddToSpreadsheet.tsx
+++ src/components/AddToSpreadsheet/AddToSpreadsheet.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { ChangeEvent, useState, useReducer, useTransition } from "react";
-import { useRouter } from "next/navigation";
+import { ChangeEvent, useMemo, useReducer } from "react";
 import Decimal from "decimal.js";
 import { Groceries } from "@/components/AddToSpreadsheet/Cells/Groceries";
 import { CoffeeTea } from "@/components/AddToSpreadsheet/Cells/CoffeeTea";
 import { Cells } from "@/components/AddToSpreadsheet/Cells/types";
+import { Payload, usePostCellData } from "@/app/hooks";
 
 interface State {
   [key: string]: { total: string | null; expense: string | null };
@@ -28,7 +28,7 @@ export type UpdateTotalAction = {
 
 export type PostExpensesAction = {
   type: ActionTypes.POST_EXPENSES;
-  payload: string;
+  payload: State;
 };
 
 export type Actions =
@@ -40,7 +40,7 @@ const reducer = (state: State, action: Actions) => {
   const { type, payload } = action;
   switch (type) {
     case ActionTypes.POST_EXPENSES:
-      return { ...state };
+      return { ...state, ...payload };
     case ActionTypes.UPDATE_EXPENSE_VALUE:
       return {
         ...state,
@@ -56,12 +56,47 @@ const reducer = (state: State, action: Actions) => {
   }
 };
 
+const defaultState: State = {
+  coffeetea: { total: null, expense: "0.00" },
+  groceries: { total: null, expense: "0.00" },
+};
+
+const preparePayload = (state: State): Payload => {
+  const validValues = Object.entries(state).filter(
+    ([cellKey, cellValues]) => cellValues.total && cellValues.expense,
+  );
+
+  const cr = validValues.map(([k]) => Cells[k as keyof typeof Cells]).join(":");
+
+  const values = validValues.map(([cellKey, cellValue]) => [
+    `=${cellValue.total && cellValue.total.replace("$", "")}+${
+      cellValue.expense && cellValue.expense.replace("$", "")
+    }`,
+  ]);
+
+  return {
+    range: `${Cells.sheetId + cr}`,
+    values,
+  };
+};
+
+const addTotals = (state: State): State => {
+  const entries = Object.entries(state);
+  return entries.reduce<State>((acm, cell) => {
+    const [key, value] = cell;
+    if (value && value.total && value.expense) {
+      let t = value.total.replace("$", "");
+      let e = value.expense.replace("$", "");
+      acm[key] = { total: `$${Decimal.add(t, e)}`, expense: "0.00" };
+    }
+    return acm;
+  }, {});
+};
+
 export const AddToSpreadsheet = () => {
-  const [loading, setLoading] = useState(false);
-  const [state, dispatch] = useReducer(reducer, {
-    coffeetea: { total: null, expense: null },
-    groceries: { total: null, expense: null },
-  });
+  const [state, dispatch] = useReducer(reducer, defaultState);
+  const payload = useMemo<Payload>(() => preparePayload(state), [state]);
+  const { runPost, loading } = usePostCellData(payload);
 
   const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
     const { value, name } = evt.target;
@@ -71,72 +106,13 @@ export const AddToSpreadsheet = () => {
     });
   };
 
-  const postData = async () => {
-    // todo: will need to pass the name
-    const path = "/api/sheets";
-
-    // in progress //
-    // body
-    const cr = Object.keys(state)
-      .map((k) => Cells[k as keyof typeof Cells])
-      .join(":");
-    const values = Object.entries(state).map(([t, e]) => [
-      `=${e.total && e.total.replace("$", "")}+${
-        e.expense && e.expense.replace("$", "")
-      }`,
-    ]);
-
-    const payload = {
-      range: `${Cells.sheetId + cr}`,
-      values,
-    };
-
-    console.log("payload ", payload);
-
-    // just groceries for the start
-    // todo: move to hooks
-    // const {
-    //   groceries: { total, expense },
-    // } = state;
-
-    // const t = total && total.length && total.replace("$", "");
-    // const e = expense && expense.length && expense.replace("$", "");
-
-    // console.log("t, e ", t, e);
-
-    // if (
-    //   t === null ||
-    //   e === null ||
-    //   Number.isNaN(Number(t)) ||
-    //   Number.isNaN(Number(e))
-    // ) {
-    //   return;
-    // }
-
-    setLoading(true);
-    try {
-      const response = await fetch(path, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        // body: `=${t}+${e}`,
-        body: JSON.stringify(payload),
-      });
-      const result = await response.json();
-      if (result.message.includes("Error")) {
-        throw new Error(result.message);
-      }
-      dispatch({
-        type: ActionTypes.UPDATE_EXPENSE_TOTAL,
-        payload: {
-          name: "groceries",
-          total: `$${Decimal.add("0.00", "7.77")}`,
-        },
-      });
-    } catch (e) {
-      throw new Error(`something did not work: ${e}`);
-    } finally {
-      setLoading(false);
-    }
+  const handlePostData = async () => {
+    await runPost();
+    // todo find better way
+    dispatch({
+      type: ActionTypes.POST_EXPENSES,
+      payload: addTotals(state),
+    });
   };
 
   return (
@@ -156,7 +132,7 @@ export const AddToSpreadsheet = () => {
           <button
             disabled={loading}
             type="button"
-            onClick={postData}
+            onClick={handlePostData}
             className="rounded h-[56px] bg-[#497236] hover:bg-[#4B952A] active:bg-[#4B952A] text-[#EDEDED] absolute bottom-40 left-6 right-6"
           >
             {loading ? "Adding" : "Add"} expense
